5a6f48f8688e37ded23055836340e5de40650df7
Kai Joseph <kai.joseph1@ontariotechu.net>
2020-10-07 14:42:35 -0400

    Added Comments

--- GDW Test File/GDW Test File.cpp
+++ GDW Test File/GDW Test File.cpp
@@ -1,6 +1,8 @@
 // GDW Test File.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+//This is a test file for our to test changes and figure out the git kracken framework
+//This file is not to be used to actually make our game.
 #include <iostream>
 
 using namespace std;
@@ -10,13 +12,4 @@ int main()
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
